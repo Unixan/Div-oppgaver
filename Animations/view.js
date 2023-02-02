a8862a6516a4ca08e00cae65611cdc36310cd769
Unixan <115973884+Unixan@users.noreply.github.com>
2023-02-02 13:50:50 +0100

    Animated Textbox

--- Animations/view.js
+++ Animations/view.js
@@ -1,7 +1,27 @@
 function mainView() {
   let html = "";
   html += /*HTML*/ `
-    <div>TEST</div>
+    <div class="animate" onclick="show()">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id odit eius veniam fugiat, laborum tempore, cumque enim sint, architecto quibusdam labore quisquam ducimus saepe distinctio! Adipisci maiores quos eaque.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id odit eius veniam fugiat, laborum tempore, cumque enim sint, architecto quibusdam labore quisquam ducimus saepe distinctio! Adipisci maiores quos eaque.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id odit eius veniam fugiat, laborum tempore, cumque enim sint, architecto quibusdam labore quisquam ducimus saepe distinctio! Adipisci maiores quos eaque.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id odit eius veniam fugiat, laborum tempore, cumque enim sint, architecto quibusdam labore quisquam ducimus saepe distinctio! Adipisci maiores quos eaque.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id odit eius veniam fugiat, laborum tempore, cumque enim sint, architecto quibusdam labore quisquam ducimus saepe distinctio! Adipisci maiores quos eaque.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id odit eius veniam fugiat, laborum tempore, cumque enim sint, architecto quibusdam labore quisquam ducimus saepe distinctio! Adipisci maiores quos eaque.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id odit eius veniam fugiat, laborum tempore, cumque enim sint, architecto quibusdam labore quisquam ducimus saepe distinctio! Adipisci maiores quos eaque.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id odit eius veniam fugiat, laborum tempore, cumque enim sint, architecto quibusdam labore quisquam ducimus saepe distinctio! Adipisci maiores quos eaque.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id odit eius veniam fugiat, laborum tempore, cumque enim sint, architecto quibusdam labore quisquam ducimus saepe distinctio! Adipisci maiores quos eaque.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id odit eius veniam fugiat, laborum tempore, cumque enim sint, architecto quibusdam labore quisquam ducimus saepe distinctio! Adipisci maiores quos eaque.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id odit eius veniam fugiat, laborum tempore, cumque enim sint, architecto quibusdam labore quisquam ducimus saepe distinctio! Adipisci maiores quos eaque.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id odit eius veniam fugiat, laborum tempore, cumque enim sint, architecto quibusdam labore quisquam ducimus saepe distinctio! Adipisci maiores quos eaque.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id odit eius veniam fugiat, laborum tempore, cumque enim sint, architecto quibusdam labore quisquam ducimus saepe distinctio! Adipisci maiores quos eaque.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id odit eius veniam fugiat, laborum tempore, cumque enim sint, architecto quibusdam labore quisquam ducimus saepe distinctio! Adipisci maiores quos eaque.
+    </div>
     `;
   document.getElementById("app").innerHTML = html;
+}
+
+function show(){
+    const box = document.querySelector(".animate")
+    box.classList.toggle("show")
 }
